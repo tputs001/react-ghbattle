77527b0f5d0b74a652cc25a463dfc78bac400c36
Tom <tputs001@gmail.com>
2016-07-05 18:42:25 -0700

    I'm adding css styling to the Home page.

--- app/config/routes.js
+++ app/config/routes.js
@@ -12,8 +12,8 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}>
       <IndexRoute component={Home} />
-      <Route path='playerOne' component={PromptContainer} />
-      <Route path='playerTwo/:playerOne' component={PromptContainer} />
+      <Route path='playerOne' header="Player One" component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header="Player Two" component={PromptContainer} />
     </Route>
   </Router>
 );
